4d6b1ac29cbdba933cd99de75fe7363a66926fc0
Jason Gilstrap <jason@uxintegrated.com>
2020-06-30 16:57:14 -0400

    Update auto_map.rb

--- lib/onesignal/auto_map.rb
+++ lib/onesignal/auto_map.rb
@@ -4,7 +4,7 @@ module OneSignal
   module AutoMap
     def create_readers **attributes
       attributes.keys.each do |k|
-        self.class.define_method k do
+        self.class.send(:define_method, k) do
           attributes[k]
         end
       end
